3160a426cf4094990609b1f46c98750d44390c7c
uhuggins <umahuggins@umas-mbp.mynet>
2019-05-29 11:50:36 +0100

    2 themes

---
+++ R/ctzn.R
@@ -0,0 +1,46 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+#install.packages("highcharter")
+
+#' CPUB theme for highcharts
+#'
+#' Based on Highcharter Google theme.
+#'
+#' @param ... Named argument to modify the theme
+#'
+#' @examples
+#'
+#' highcharts_demo() %>%
+#'   hc_add_theme(hc_theme_ctzn())
+#'
+#' @export
+hc_theme_ctzn <- function(...) {
+  hc_theme_merge(
+    hc_theme_google(),
+    hc_theme(
+      chart = list(
+        backgroundColor = "transparent"),
+      title = list(style = list(fontFamily = "Georgia")),
+      subtitle = list(style = list(fontFamily = "Georgia")),
+      yAxis = list(title = list(style = list(fontFamily = "Georgia")),
+                   labels = list(style = list(fontFamily = "Georgia"))),
+      xAxis = list(title = list(style = list(fontFamily = "Georgia")),
+                   labels = list(style = list(fontFamily = "Georgia")))
+
+    ))
+
+}
