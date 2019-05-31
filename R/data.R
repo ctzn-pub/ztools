013686921f613e2f1c7ee8fed1bfc2368eccb805
uhuggins <uhuggins>
2019-05-31 19:06:48 +0100

    adding map data

--- R/data.R
+++ R/data.R
@@ -2,3 +2,8 @@ library(rio)
 library(usethis)
 countrydata<- import("/Users/umahuggins/Dropbox/ON/Projects/World/Meta/data/country_meta_use.rds")
 use_data(countrydata, overwrite = TRUE)
+
+
+
+globalMapData<- import("./../../Projects/World/visualization/globalMapData.rds")
+use_data(globalMapData, overwrite = TRUE)
