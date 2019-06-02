8657f46c8cc101ca25e261928878ae5d72641e5f
uhuggins <uhuggins>
2019-06-02 15:40:44 +0100

    fix?

--- R/data.R
+++ R/data.R
@@ -1,13 +1,13 @@
 library(rio)
 library(usethis)
-countrydata<- import("/Users/umahuggins/Dropbox/ON/Projects/World/Meta/data/country_meta_use.rds")
-use_data(countrydata, overwrite = TRUE)
+#countrydata<- import("/Users/umahuggins/Dropbox/ON/Projects/World/Meta/data/country_meta_use.rds")
+#use_data(countrydata, overwrite = TRUE)
 
 
 
-globalMapData<- import("/Users/umahuggins/Dropbox/ON/Projects/World/visualization/globalMapData.rds")
-use_data(globalMapData, overwrite = TRUE)
+#globalMapData<- import("/Users/umahuggins/Dropbox/ON/Projects/World/visualization/globalMapData.rds")
+#use_data(globalMapData, overwrite = TRUE)
 
 #From https://unstats.un.org/unsd/methodology/m49/overview/
-UNSD<- import("/Users/umahuggins/Dropbox/ON/Projects/World/Meta/data/UNSD — Methodology.xlsx")
-use_data(UNSD, overwrite = TRUE)
+#UNSD<- import("/Users/umahuggins/Dropbox/ON/Projects/World/Meta/data/UNSD — Methodology.xlsx")
+#use_data(UNSD, overwrite = TRUE)
