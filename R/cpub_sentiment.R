21a459440b09a46336493c973cc7142a9d3a7741
uhuggins <uhuggins>
2019-11-26 13:39:58 -0500

    fixging up sentiment fxn

--- R/cpub_sentiment.R
+++ R/cpub_sentiment.R
@@ -1,5 +1,8 @@
-#' Augment a data frame to include text sentiments from bing, afinn, NRC
+#' Augment a data frame to include text sentiments from  afinn, NRC
 #'
+#'GI, QDAP
+#'Harvard-IV dictionary (GI) http://www.wjh.harvard.edu/~inquirer/spreadsheet_guide.htm
+#'QDAP dictionary from the package qdapDictionaries
 #' @param df
 #'
 #' Data frame to work with
@@ -21,6 +24,12 @@ cpub_sentiment<- function(df, text_column){
   ##See if the text has a link in it
   df <- df %>%  mutate(contains_url = ifelse(grepl("http", !!sym(text_column)), 1, 0))
   df$uuid<- rep(1:nrow(df))
+  df<-df%>% mutate(sentimentGI = analyzeSentiment(df[text_column])$SentimentGI,
+                   negativityGI = analyzeSentiment(df[text_column])$NegaativityGI,
+                   positivityGI = analyzeSentiment(df[text_column])$PositivityGI,
+                   sentimentQDAP = analyzeSentiment(df[text_column])$SentimentQDAP,                              negativityQDAP = analyzeSentiment(egdf$documents)$NegaativityQDAP,
+                   positivityQDAP = analyzeSentiment(df[text_column])$PositivityQDAP)
+
   library(tidyr)
   ##Use tidytext to unnest tokens
   library(tidytext)
@@ -35,24 +44,21 @@ cpub_sentiment<- function(df, text_column){
     anti_join(stop_words)
 
   ###Get 'sentiment' (bing)
-  bing <- text_df %>%
-    inner_join(get_sentiments("bing")) %>%
-    count(uuid, sentiment)%>%
-    spread(sentiment, n, fill = 0)
+ #  bing <- text_df %>%
+ #    inner_join(get_sentiments("bing")) %>%
+ #    count(uuid, sentiment)%>%
+ #    spread(sentiment, n, fill = 0) %>%
  #   mutate(bing_sentiment = positive - negative)
-
-#  bing<-bing %>% mutate(bing_negative = negative,
+ #
+ # bing<-bing %>% mutate(bing_negative = negative,
  #                       bing_positive = positive)
-#  bing<-select(bing, -negative, - positive)
+ # bing<-select(bing, -negative, - positive)
 
   ## Get AFINN sentiment
   afinn <- text_df %>%
     inner_join(get_sentiments("afinn")) %>%
     group_by(uuid) %>%
     summarise(afinn_sentiment=  mean(value))
-
-
-
   single_nrc<-get_sentiments("nrc")
   single_nrc<-single_nrc %>% select(word)
   single_nrc<- unique(single_nrc)
@@ -65,11 +71,16 @@ emotion_share<-emotion_share %>% group_by(uuid) %>%mutate(emotion = sum(!is.na(e
 emotion_share<-emotion_share %>% group_by(uuid) %>%mutate(emotion_share = emotion/nwords)
 emotion_share<- emotion_share %>% select(-word)
 emotion_share<- unique(emotion_share)
+
   nrc <- text_df %>%
     inner_join(get_sentiments("nrc")) %>%
     count(uuid,sentiment)%>%
-    spread(sentiment, n, fill = 0) %>%
-    group_by(uuid) %>%
+    spread(sentiment, n, fill = 0)
+
+  missing<- setdiff( c("anger","anticipation","disgust","fear","joy","sadness","surprise","trust"),names(nrc) )
+  nrc[,missing] <- 0
+
+  nrc <-nrc %>% group_by(uuid) %>%
     mutate(emotion=sum(anger+anticipation+disgust+fear+joy+sadness+surprise+trust))%>%
     mutate(Per_anger= anger/emotion)%>%
     mutate(Per_anticipation= anticipation/emotion)%>%
@@ -79,42 +90,19 @@ emotion_share<- unique(emotion_share)
     mutate(Per_sadness=sadness/emotion)%>%
     mutate(Per_surprise=surprise/emotion)%>%
     mutate(Per_trust=trust/emotion)
+nrc<- select(nrc, -emotion, - c("anger","anticipation","disgust","fear","joy","sadness","surprise","trust"))
 
-  df<- left_join(df, afinn, by = "uuid")
-  df<- left_join(df, bing,  by = "uuid")
+
+df<- left_join(df, afinn, by = "uuid")
+#  df<- left_join(df, bing,  by = "uuid")
   df<- left_join(df, nrc, by = "uuid")
   df<- left_join(df, count, by = "uuid")
   df<- left_join(df, emotion_share, by = "uuid")
 
-  df
 
+
+df
 }
 
-# cpub_sentiment(egdf, text_column = 'documents')
-#
-#
-# documents <- c("This is a good thing!",
-#                "This is a very good thing!",
-#                "This is okay.",
-#                "This is a bad thing.",
-#                "This is a very bad thing.")
-#
-# nos<- c(1,22,3,4,5)
-#
-# egdf<-data.frame(nos, documents)
-# egdf$documents<-as.character(egdf$documents)
-# sentiment <- egdf %>% mutate(sentimentGI = analyzeSentiment(egdf$documents)$SentimentGI,
-#                              negativityGI = analyzeSentiment(egdf$documents)$NegaativityGI,
-#                              positivityGI = analyzeSentiment(egdf$documents)$PositivityGI,
-#                              sentimentQDAP = analyzeSentiment(egdf$documents)$SentimentQDAP,
-#                              negativityQDAP = analyzeSentiment(egdf$documents)$NegaativityQDAP,
-#                              positivityQDAP = analyzeSentiment(egdf$documents)$PositivityQDAP
-#
-#                              )
+cpub_sentiment(egdf, text_column = 'documents')
 
-#GI, HE, LM, QDAP
-
-#Harvard-IV dictionary (GI) http://www.wjh.harvard.edu/~inquirer/spreadsheet_guide.htm
-#Henry’s Financial dictionary (HE)
-#Loughran-McDonald Financial dictionary (LM)
-#QDAP dictionary from the package qdapDictionaries
